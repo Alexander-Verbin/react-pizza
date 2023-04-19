38a5108a66de7ef8f63795a7fabaf6724fc6c721
Alexander-Verbin <sash.verbin@yandex.ru>
2023-04-19 16:01:02 +0300

    12 lesson done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState,createContext} from "react";
 
 import {Route, Routes} from "react-router-dom";
+import {useSelector,useDispatch} from "react-redux";
 
 import "./scss/app.scss"
 
